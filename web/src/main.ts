8944e51311233917d02893b3d7a09d6c889a8c6b
vietcanhvn <vietcanhvn@gmail.com>
2026-09-09 17:33:07 +0700

    build(web): add vue-i18n i18n infrastructure

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,10 +6,11 @@ import App from './App.vue'
 import AppButton from './components/AppButton.vue'
 import router from './router'
 import { initializeAppTheme } from './shared/appTheme'
+import i18n from './locales'
 import './styles.css'
 import './features/workbench/styles/workbench.css'
 import './features/workbench/styles/shengshimedia-workbench.css'
 import './app-theme.css'
 
 initializeAppTheme()
-createApp(App).component('AppButton', AppButton).use(createPinia()).use(router).mount('#app')
+createApp(App).component('AppButton', AppButton).use(createPinia()).use(router).use(i18n).mount('#app')
